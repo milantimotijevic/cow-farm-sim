721433a2ed4807c4710b25ab3120838a9f182641
mtimo101289@gmail.com <mtimo101289@gmail.com>
2021-08-15 18:10:53 +0200

    add examples

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const { Farm } = require('./entities');
 const farm = new Farm();
 
 farm.giveBirth(0, 'Nessie')
-
+farm.giveBirth(0, 'Lessie')
+farm.giveBirth(0, 'Sarah')
+farm.giveBirth(0, 'Milka')
+farm.giveBirth(0, 'Tina')
+farm.giveBirth(1, 'Mina')
+farm.giveBirth(7, 'Dina')
 farm.printAll();
 
 
